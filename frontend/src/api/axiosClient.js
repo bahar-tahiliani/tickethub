0c911f76a3cc23baf758ad0cdc74039eb6c1dd07
Bahar Tahiliani <bahar.tahiliani2023@vitstudent.ac.in>
2026-08-24 03:46:04 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+VITE_API_URL=https://tickethub-backend-rdgk.onrender.com/api
 
 const client = axios.create({ baseURL: API_URL });
 
